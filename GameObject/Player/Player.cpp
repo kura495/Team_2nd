b14ffdcfdafbeea55b25ef3826e96792c048b283
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-09-08 10:38:34 +0900

    Playerの移動処理の変更

--- GameObject/Player/Player.cpp
+++ GameObject/Player/Player.cpp
@@ -24,14 +24,35 @@ void Player::Update()
 			Vector3 move = {
 				(float)joyState.Gamepad.sThumbLX / SHRT_MAX, 0.0f,
 				(float)joyState.Gamepad.sThumbLY / SHRT_MAX };
-			move.x = Normalize(move).x * moveSpeed;
-			move.y = Normalize(move).y * moveSpeed;
-			move.z = Normalize(move).z * moveSpeed;
+
+			if (Length(move) > threshold)
+			{
+				isMove = true;
+			}
+
+			if (isMove == true)
+			{
+				//反映
+				move.x = Normalize(move).x * moveSpeed;
+				move.y = Normalize(move).y * moveSpeed;
+				move.z = Normalize(move).z * moveSpeed;
+				
+				//目標角度の算出
+				angle_ = std::atan2(move.x, move.z);
+
+			}
+			//移動量
 			worldTransform_.translation_.x = worldTransform_.translation_.x + move.x;
 			worldTransform_.translation_.y = worldTransform_.translation_.y + move.y;
 			worldTransform_.translation_.z = worldTransform_.translation_.z + move.z;
+
+			//// Y軸周り角度(θy)	歩いている方向に顔を向ける
+			//worldTransform_.rotation_.y = LerpShortAngle(worldTransform_.rotation_.y, angle_, 0.1f);
+
+
 		}
 	}
+
 	if (input->IspushKey(DIK_W)) {
 		worldTransform_.translation_.y += 0.5f * speed;
 	}
@@ -85,3 +106,42 @@ void Player::ImGui()
 	ImGui::SliderFloat3("scale", &worldTransform_.scale_.x,-10, 10);
 	ImGui::End();
 }
+
+
+float Player::Lerp(const float& a, const float& b, float t) {
+	float result{};
+
+	result = a + b * t;
+
+	return result;
+}
+
+// 最短角度補間
+float Player::LerpShortAngle(float a, float b, float t)
+{
+	// 角度差分を求める
+	float diff = b - a;
+
+	diff = std::fmod(diff, 2 * (float)std::numbers::pi);
+	if (diff < 2 * (float)-std::numbers::pi)
+	{
+		diff += 2 * (float)std::numbers::pi;
+	}
+	else if (diff >= 2 * std::numbers::pi)
+	{
+		diff -= 2 * (float)std::numbers::pi;
+	}
+
+	diff = std::fmod(diff, 2 * (float)std::numbers::pi);
+	if (diff < (float)-std::numbers::pi)
+	{
+		diff += 2 * (float)std::numbers::pi;
+	}
+	else if (diff >= (float)std::numbers::pi)
+	{
+		diff -= 2 * (float)std::numbers::pi;
+	}
+
+	return Lerp(a, diff, t);
+}
+
--- GameObject/Player/Player.h
+++ GameObject/Player/Player.h
@@ -5,6 +5,7 @@
 #include "Transform.h"
 #include "Input.h"
 #include "VectorCalc.h"
+#include "Base/Math/MatrixCalc.h"
 
 class Player
 {
@@ -16,11 +17,18 @@ private:
 	void ApplyGlobalVariables();
 	void ImGui();
 
+	float Lerp(const float& a, const float& b, float t);
+	float LerpShortAngle(float a, float b, float t);
+
 	Input* input = nullptr;
 	Model* model = nullptr;
 	GlobalVariables* globalVariables = nullptr;
 	WorldTransform worldTransform_;
 	float speed = 0.0f;
 	XINPUT_STATE joyState;
+
+	bool isMove = false;	//プレイヤーが移動しているか　false:移動していない
+	const float threshold = 0.7f;	//しきい値
+	float angle_;	//目標角度
 };
 
